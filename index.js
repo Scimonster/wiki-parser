ff4cbedd6a3ace42e73044df31d7df960468556c
Scimonster <tehalmightyscimonster@gmail.com>
2014-03-17 22:55:30 +0200

    Start work on simple lists.

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ exports.samples = [
 	'<span>pre</span><div id="id"><p><div class="class">mid</div><div class="class">again</div></p></div><div id="second"></div><span>post</span>',
 	'[[link]] <nowiki>[[not a link]]<nomarkup><nowiki>~~~~</nowiki></nomarkup></nowiki> <nomarkup>~~~~</nomarkup> real sign ~~~~ <nowiki>~~~~</nowiki>',
 	'<h1>head</h1>content<h4>sub</h4><h2 title="attr"></h2>',
-	'<nowiki allow="sign">[[link target|title]]\nsign: ~~~~\nescape markup: <nowiki>markup</nomarkup></nowiki>'
+	'<nowiki allow="sign">[[link target|title]]\nsign: ~~~~\nescape markup: <nowiki>markup</nomarkup></nowiki>',
+	'*1\n*2\n**2.1\n**2.2\n*3',
+	'#a\n#b\n##ba\n##bb\n#c',
+	'*1\n**2\n* 3\n*#4\n# 5\n##6\n#*7'
 ];
 
 var defaults = exports.defaults = {
@@ -49,14 +52,12 @@ function parser(text, opts) {
 				line.replace(/'''([^''']+)'''/g, html.createTag('strong', {}, '$1')). // bold
 				replace(/''([^'']+)''/g, html.createTag('em', {}, '$1'))
 			)).map(function(head){ // create headers
-				if (head.level) {
+				if (head.level) { // a header
 					return html.createTag('h' + head.level, opts.headerHTML, head.name);
-				} else {
+				} else { // do start-of-line stuff (lists, pre)
 					return head.text;
 				}
-			}).map(function(line){ // start of line actions
-				return line;
-			});
+			}).join('');
 		}
 	}).join('');
 }
@@ -131,6 +132,76 @@ sign.def = function(name) {
 
 exports.sign = sign;
 
+
+function lists(text) {
+	// unordered list - *
+	// ordered list - #
+	// definition list - ;:
+	var tags = {
+		'*': ['ul', 'li'],
+		'#': ['ol', 'li'],
+		';': ['dl', 'dt'],
+		':': ['dl', 'dd'],
+		'' : null
+	}, prefix = '';
+
+	function wrapArray(val) {
+		return _.isArray(val) ? val : [val];
+	}
+
+	function down(prefix) {
+		return wrapArray(prefix).reverse().map(function(close){
+			return '</' + tags[close][1] + '></' + tags[close][0] + '>';
+		}).join('');
+	}
+
+	function up(prefix) {
+		return wrapArray(prefix).map(function(close){
+			return '<' + tags[close][0] + '><' + tags[close][1] + '>';
+		}).join('');
+	}
+
+	function diff(prefix, current) {
+		if (prefix[0] == current[0]) {
+			return diff(prefix.slice(1), current.slice(1));
+		} else {
+			return [prefix, current];
+		}
+	}
+
+	function compare(a, b) {
+		return a.slice(0, b.length) == b;
+	}
+
+	function crop(a, b) {
+		return _.last(a, a.length - b.length);
+	}
+
+	function itemTag(symbol) {
+		return tags[_.last(symbol)][1];
+	}
+
+	return (_.isArray(text) ? text : text.toString().split('\n')).concat('').map(function(line){
+		var start = line.match(/^([\*#\:;]*)\s*(.*)$/), ret = line, tmp;
+		if (prefix == start[1]) { // same as last time
+			if (prefix) {
+				ret = '</' + itemTag(start[1]) + '><' + itemTag(start[1]) + '>' + start[2];
+			} else {
+				ret = ' ' + start[2]; // no prefix, so just add the line
+			}
+		} else if (compare(prefix, start[1])) { // going down a level or so
+			ret = down(crop(prefix, start[1])) + (tags[_.last(start[1])] ? '</' + itemTag(start[1]) + '><' + itemTag(start[1]) + '>' : '') + start[2];
+		} else if (compare(start[1], prefix)) { // going up a level or so
+			ret = up(crop(start[1], prefix)) + start[2];
+		} else { // down, then up
+			tmp = diff(prefix, start[1]);
+			ret = down(tmp[0].split('')) + up(tmp[1].split('')) + start[2];
+		}
+		prefix = start[1];
+		return ret;
+	}).join('');
+}
+exports.lists = lists;
 
 
 //exports.html = html;
